838b36dac0b67e8dfaa40d1b58efbb3d51bf885e
miha53cevic <44951515+miha53cevic@users.noreply.github.com>
2019-04-21 16:12:59 +0200

    added ui

--- Spirograph.js
+++ Spirograph.js
@@ -17,6 +17,8 @@ class Spirograph {
         // Angle
         this.t = 0;
 
+        this.period = 6.01 * Math.PI;
+
         // Shape made by the spirograph
         this.shape = [];
 
@@ -24,21 +26,26 @@ class Spirograph {
 
         console.log("l = " + this.l);
         console.log("k = " + this.k);
+
+        // Show certain parts
+        this.showCo = true;
+        this.showCi = true;
+        this.showCiPath = false;
     }
 
     display() {
         clear('white');
         // Display outerCircle
-        drawArc(WIDTH / 2, HEIGHT / 2, this.R, 0, 2 * Math.PI, 'blue');
+        if (this.showCo)
+            drawArc(WIDTH / 2, HEIGHT / 2, this.R, 0, 2 * Math.PI, 'blue');
 
         // Calculate shape point
         let x = this.R * ((1 - this.k) * Math.cos(this.t) + this.l * this.k * Math.cos((1 - this.k) / this.k * this.t)) + WIDTH / 2;
         let y = this.R * ((1 - this.k) * Math.sin(this.t) - this.l * this.k * Math.sin((1 - this.k) / this.k * this.t)) + HEIGHT / 2;
 
-        if (this.t <= 6.01 * Math.PI)
-        {
+        if (this.t <= this.period) {
             this.shape.push(new Point(x, y));
-        } 
+        }
 
         // Dodaj kutu
         this.t += 0.02;
@@ -51,10 +58,12 @@ class Spirograph {
         let sy = (HEIGHT / 2) + ((this.R - this.r) * Math.sin(this.t));
 
         // Draw circle on which Ci rotates on
-        drawArc(WIDTH / 2, HEIGHT / 2, this.R - this.r, 0, 2 * Math.PI, 'yellow');
+        if (this.showCiPath)
+            drawArc(WIDTH / 2, HEIGHT / 2, this.R - this.r, 0, 2 * Math.PI, 'yellow');
 
         // Draw Ci which moves on Circle(sx, sy)
-        drawArc(sx, sy, this.r, 0, 2 * Math.PI, 'green');
+        if (this.showCi)
+            drawArc(sx, sy, this.r, 0, 2 * Math.PI, 'green');
 
         // Draw radius line of Ci as it rotates and draws shape
         line(sx, sy, x, y, 'red', 2);
--- main.js
+++ main.js
@@ -1,5 +1,17 @@
 let app;
 
+function startApp(R = 200, r = 75, p = 30) {
+    delete app;
+    app = new Spirograph(R, r, p);
+
+    app.period = document.getElementById('t').value * Math.PI;
+
+    document.getElementById('showK').textContent = "k = " + app.k;
+    document.getElementById('showL').textContent = "l = " + app.l;
+
+    window.requestAnimationFrame(loop);
+}
+
 function loop() {
     app.display();
     window.requestAnimationFrame(loop);
@@ -9,7 +21,37 @@ function loop() {
 
 window.onload = function () {
     createCanvas(400, 400);
-    app = new Spirograph(200, 75, 30);
+    startApp();
+}
 
-    window.requestAnimationFrame(loop);
+// Checkbox button functions
+function showCi() {
+    if (document.getElementById('Ci').checked)
+        app.showCi = true;
+    else app.showCi = false;
+}
+
+function showCo() {
+    if (document.getElementById('Co').checked)
+        app.showCo = true;
+    else app.showCo = false;
+}
+
+function showCiPath() {
+    if (document.getElementById('CiPath').checked)
+        app.showCiPath = true;
+    else app.showCiPath = false;
+}
+
+// Slider Functions
+function SliderChange() {
+    let r = document.getElementById('r').value;
+    let R = document.getElementById('R').value;
+    let p = document.getElementById('p').value;
+
+    startApp(R, r, p);
+}
+
+function PeriodChange() {
+    SliderChange();
 }
